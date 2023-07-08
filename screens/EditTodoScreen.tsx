bfd7d10567fb188673279db552b97596d6a22cfa
looper147 <hadismsmanii@gmail.com>
2023-07-08 15:02:26 +0300

    sc

--- screens/EditTodoScreen.tsx
+++ screens/EditTodoScreen.tsx
@@ -6,19 +6,24 @@ import { useState } from "react";
 //to access passed todo object from the home screen
 import { useRoute } from "@react-navigation/native";
 
+interface Todo {
+  completed: boolean;
+  id: number;
+  text: string;
+}
 const EditTodoScreen = () => {
   // const todos = useAppSelector((state) => state.todo.todos);
   const route = useRoute();
-  const todo = route.params?.todo;
+  const todo = route.params?.todo as Todo;
   const [editTodo, setEditTodo] = useState(todo?.text || "");
-
+  console.log(todo);
   return (
     <View style={{ padding: 10, margin: 10 }}>
       <TextInput
         value={editTodo}
         onChangeText={setEditTodo}
         mode="flat"
-        style={{ fontSize: 29, padding: 100 }}
+        style={{ fontSize: 20, padding: 100 }}
       />
     </View>
   );
